240dc318ea4d58aedc031296a29a99b3f2bbee16
wendiNurhermansah <nurhermansahwendi@gmail.com>
2022-12-13 13:59:03 +0700

    push two

---
+++ app/Bank_sampah.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Bank_sampah extends Model
+{
+    protected $table = 'tm_bank_sampah';
+    protected $guarded = [];
+}
---
+++ app/Tps3r.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Tps3r extends Model
+{
+    protected $table = 'tm_tps3r';
+    protected $guarded = [];
+}
