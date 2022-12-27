58f81c366790dfcbb5789219444c2425452d968b
wendiNurhermansah <nurhermansahwendi@gmail.com>
2022-12-27 11:13:58 +0700

    halaman login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -31,7 +31,7 @@
             </div>
             <div class="col-md-3 white">
                 <div class="pl-5 pt-5 pr-5 m-t-90 pb-0">
-                    <img src="{{asset('images/icon_sampah.png')}}" class="mx-auto d-block" width="150" alt=""/>
+                    <img src="{{asset('images/logo_bank_sampah.png')}}" class="mx-auto d-block" width="250" alt=""/>
                 </div>
                 <div class="p-5">
                     <h3 class="font-weight-normal">Selamat Datang</h3>
